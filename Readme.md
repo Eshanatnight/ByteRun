e376bb4ae4279fd8741e778633fe0cb9d5f4a859
Eshan Chatterjee <eshan2001221@gmail.com>
2022-04-10 17:03:26 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Byterun
+
+This is a pure-Python implementation of a Python bytecode execution virtual
+machine.  I started it to get a better understanding of bytecodes.
---
+++ byterun.py
@@ -0,0 +1,12 @@
+# !/bin/python3
+
+
+from distutils.core import setup
+
+setup(
+    name='Byterun',
+    version='1.0',
+    description='Pure-Python Python bytecode execution',
+    packages=['byterun'],
+    install_requires=['six'],
+    )
